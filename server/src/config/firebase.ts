cf3f7c8ef051b371a5e1640cf514a952a558c197
Kanishak todwal <146489713+Kannu20@users.noreply.github.com>
2026-03-03 21:30:17 +0530

    Update Firebase config to use environment variables
    Refactor Firebase service account configuration to use environment variables instead of a JSON file.

--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -15,22 +15,33 @@
 // export default admin;
 
 import admin from "firebase-admin";
-import path from "path";
+// import path from "path";
 import fs from "fs";
 
-const serviceAccountPath = path.join(
-  __dirname,
-  "firebaseServiceAccount.json"
-);
+const serviceAccountPath = {
+  type: process.env.FB_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FB_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FB_CLIENT_ID,
+  auth_uri: process.env.FB_AUTH_URI,
+  token_uri: process.env.FB_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FB_CLIENT_CERT_URL,
 
-if (!fs.existsSync(serviceAccountPath)) {
-  throw new Error("🔥 firebaseServiceAccount.json not found");
-}
+};
+
+// if (!fs.existsSync(serviceAccountPath)) {
+//   throw new Error("🔥 firebaseServiceAccount.json not found");
+// }
 
 admin.initializeApp({
   credential: admin.credential.cert(
-    require(serviceAccountPath)
+    // require(serviceAccountPath)
+    serviceAccountPath as admin.ServiceAccount
   ),
 });
 
 export default admin;
+
