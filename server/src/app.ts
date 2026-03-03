8532741f79b0a908a7b9036041da30166e581256
Kanishak todwal <kannutodwal@gmail.com>
2026-03-03 22:54:58 +0530

    commit

--- server/src/app.ts
+++ server/src/app.ts
@@ -22,7 +22,10 @@ app.use(express.urlencoded({ extended: true }));
 ===================== */
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://career-compass-flame.vercel.app"
+    ],
     credentials: true,
   })
 );
